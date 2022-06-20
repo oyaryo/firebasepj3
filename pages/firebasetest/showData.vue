ff5e360361c1b6b104b95039203ce4809c6f07ac
oyaryo <file_604@yahoo.co.jp>
2022-06-20 21:28:02 +0900

    add remove product in products

--- pages/firebasetest/showData.vue
+++ pages/firebasetest/showData.vue
@@ -5,16 +5,27 @@
         <v-card-title>商品一覧</v-card-title>
       </div>
       <div v-for="product in products" :key="product.id">
-        商品名: {{ product.name }} / 概要： {{ product.description }} / 価格：{{
-          product.price
-        }}<hr />
+        ID: {{ product.id }}<br />
+        商品名: {{ product.name }}<br />
+        概要： {{ product.description }}<br />
+        価格：{{ product.price }}
+        <div class="text-right">
+          <v-btn color="error" @click="remove(product.id)">削除</v-btn>
+        </div>
+        <hr />
       </div>
     </v-card>
   </div>
 </template>
 
 <script>
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 // import { getDownloadURL, getStorage, ref } from "firebase/storage";
 
 export default {
@@ -28,11 +39,30 @@ export default {
       const db = getFirestore(this.$firebase);
       const querySnapshot = await getDocs(collection(db, "products"));
       querySnapshot.forEach((doc) => {
-        this.products.push(doc.data());
+        const data = doc.data();
+        this.products.push({
+          id: doc.id,
+          name: data.name,
+          description: data.description,
+          price: data.price,
+        });
       });
     } catch (e) {
       console.error("error: ", e);
     }
+  },
+  methods: {
+    async remove(id) {
+      const db = getFirestore(this.$firebase);
+      await deleteDoc(doc(db, "products", id))
+      .then((result) => {
+        console.log("削除完了");
+        location.reload();
+      })
+      .catch((e) => {
+        console.error('error: ', e);
+      });
+    },
   },
 };
 </script>
