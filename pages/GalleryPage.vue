ddc377be773dce111e45a673c5dbebee831f0236
oyaryo <file_604@yahoo.co.jp>
2022-07-12 23:09:33 +0900

    Revert "add vue-unity"
    This reverts commit 7827a59e0b72cdd8b8d0c608abab56071c82c2a0.

--- pages/GalleryPage.vue
+++ pages/GalleryPage.vue
@@ -1,25 +1,12 @@
 <template>
-  <div>
-    <script src="<%= htmlWebpackPlugin.files.publicPath %>unitybuild/TemplateData/UnityProgress.js"></script>
-    <script src="<%= htmlWebpackPlugin.files.publicPath %>unitybuild/Build/UnityLoader.js"></script>
-    <v-container>
-      <div class="flex">
-        <div class="w-3/12 h-24 bg-meta-gallery-100"></div>
-        <div class="w-3/12 h-24 bg-meta-gallery-200"></div>
-        <div class="w-3/12 h-24 bg-meta-gallery-300"></div>
-        <div class="w-3/12 h-24 bg-meta-gallery-400"></div>
-      </div>
-      <unity
-        src="unitybuild/Build/unitybuild.json"
-        v-bind="{
-          width: 640,
-          height: 480,
-          hideFooter: true,
-          externalProgress: true,
-          module: { TOTAL_STACK: 6 * 1024 * 1024 },
-        }"
-        unityLoader="unitybuild/Build/UnityLoader.js"
-      ></unity>
-    </v-container>
-  </div>
+<div>
+  <v-container>
+    <div class="flex">
+      <div class="w-3/12 h-24 bg-meta-gallery-100"></div>
+      <div class="w-3/12 h-24 bg-meta-gallery-200"></div>
+      <div class="w-3/12 h-24 bg-meta-gallery-300"></div>
+      <div class="w-3/12 h-24 bg-meta-gallery-400"></div>
+    </div>
+  </v-container>
+</div>
 </template>
